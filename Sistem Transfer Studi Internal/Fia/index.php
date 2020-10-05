b051a9b1cecd2931b19d80f539847b5ad08ee36f
FiaDess <agustinusfajarpasila@gmail.com>
2020-10-05 10:44:39 +0700

    nambah mpdf
    monkaS

--- Sistem Transfer Studi Internal/Fia/index.php
+++ Sistem Transfer Studi Internal/Fia/index.php
@@ -11,63 +11,114 @@
 <body>
 
 
-	<div class="container mt-5">
-		<form>
-			<h1>Create your own PDF</h1>
+	<div class="container mt-5 col-md-6">
+		
+		<form action="makepdf.php" method="post">
+
+			<h1>Form Transfer Progdi</h1>
 			<p>Isi form dengan data yang benar</p>	
 
-			<div class="row">
+			<div class="row col-md-12">
 				
-				<label for="usr">Nama lengkap</label>
 				<div class="col-md-6">
+				<label for="usr">Nama lengkap</label>
 					<input type="text" name="name" placeholder="Ex. Andreas Devanus Prasetya" class="form-control" required>
 				</div>
 
-				<label for="usr">NIM</label>
 				<div class="col-md-6">
+				<label for="usr">NIM</label>
 					<input type="text" name="nim" placeholder="Ex. 21.N1.0001" class="form-control" required>
+				</div>
+
+			</div>
+
+			<div class="col-md-11">
+
+				<label for="usr">Alamat (KTP)</label>
+				<input type="text" name="alamat" placeholder="Ex. JL. Graha Merdeka II No. 4" class="form-control" required>
+				
+			</div>
+
+			<div class="row col-md-12">
+				
+				<div class="col-md-6">
+					<label for="usr">Kota</label>
+					<input type="text" name="kota" placeholder="Ex. Jakarta" class="form-control" required>
+				</div>
+
+				<div class="col-md-6">
+					<label for="usr">No Handphone</label>
+					<input type="text" name="nohp" placeholder="Ex. 08941xxxxxxx" class="form-control" required>
+				</div>
+
+			</div>
+
+			<div class="row col-md-12">
+
+				<div class="col-md-6">
+					<label for="usr">Tahun Akademik</label>
+					<input type="text" name="takademik" placeholder="Tahun Akademik" class="form-control" required>
+				</div>
+
+
+				<div class="col-md-6">
+
+					<label for="usr">Semester</label>
+					</br>
+
+					<form>
+
+					    <label class="radio-inline">
+					     	<input type="radio" name="semester">Ganjil
+					    </label>&emsp;
+
+					    <label class="radio-inline">
+					     	<input type="radio" name="semester">Genap
+					    </label>
+
+					</form>
+					</br>
+
+				</div>
+
+			</div>
+
+			
+			<div class="row col-md-12">
+
+				<div class="col-md-6">
+					<div class="form-group">
+
+						<label for="pAsal">Fakultas/Prodi Asal</label>
+				      	<select class="form-control" name="pAsal" required>
+					        <option>Sistem Informasi</option>
+					        <option>Game Technology</option>
+					        <option>Teknik Infromatika</option>
+					        <option>Akuntasi</option>
+				      </select>
+
+					</div>
+				</div>
+
+				<div class="col-md-6">
+				<div class="form-group">
+
+					<label for="pTujuan">Fakultas/Prodi Tujuan</label>
+			      	<select class="form-control" name="pTujuan" required>
+				        <option>Sistem Informasi</option>
+				        <option>Game Technology</option>
+				        <option>Teknik Infromatika</option>
+				        <option>Akuntasi</option>
+			      </select>
+
+				</div>
 				</div>
 			</div>
 
-			<label for="usr">Alamat (KTP)</label>
-			<input type="text" name="alamat" placeholder="Ex. JL. Graha Merdeka II No. 4" class="form-control" required>
-
-			<label for="usr">Kota</label>
-			<input type="text" name="kota" placeholder="Ex. Jakarta" class="form-control" required>
-
-			<label for="usr">No Handphone</label>
-			<input type="number" name="nohp" placeholder="Ex. 08941xxxxxxx" class="form-control" required>
-
-			<label for="usr">Semester</label>
-			<label class="radio-inline"><input type="radio" name="sGanjil" class="form-control" required>Ganjil</label>
-			<label class="radio-inline"><input type="radio" name="sGenap" class="form-control" required>Genap</label>
-			
-
-			<input type="text" name="takademik" placeholder="Tahun Akademik" class="form-control" required>
-
-			
-			//Dropdown options
-			<div class="form-group">
-				<label for="pAsal">Fakultas/Prodi Asal</label>
-		      	<select class="form-control" name="pAsal" required>
-			        <option>Sistem Informasi</option>
-			        <option>Game Technology</option>
-			        <option>Teknik Infromatika</option>
-			        <option>Akuntasi</option>
-		      </select>
+			<div class="offset-md-6">
+				<button type="submit" class="btn btn-success">Submit</button>
 			</div>
-
-			<div class="form-group">
-				<label for="pTujuan">Fakultas/Prodi Tujuan</label>
-		      	<select class="form-control" name="pTujuan" required>
-			        <option>Sistem Informasi</option>
-			        <option>Game Technology</option>
-			        <option>Teknik Infromatika</option>
-			        <option>Akuntasi</option>
-		      </select>
-			</div>
-
-			<button type="submit" class="btn btn-success">Submit</button>
+			
 		</form>
 	</div>
 
